9a420dc702c818fc24e2778172bd4e8d2c109591
Rohan Shetty <rohan.shetty@veon.com>
2017-07-21 10:49:25 +0200

    Using xml files for ignite config

--- src/main/java/poc/CacheTest.java
+++ src/main/java/poc/CacheTest.java
@@ -11,16 +11,21 @@ import org.apache.ignite.binary.BinaryObject;
 public class CacheTest {
 
     public static void main (String[] args) throws Exception {
-        IgniteConfig cacheTest = new IgniteConfig();
-        SegmentBinaryMapper mapper = new SegmentBinaryMapper();
-        Ignite ignite = (Ignition.getOrStart(cacheTest.igniteConfiguration()));
 
-        IgniteCache<String,BinaryObject> cache = ignite.getOrCreateCache(cacheTest.getAllCacheConfigurations()[0].setStoreKeepBinary(true)).withKeepBinary();
-        Segment segment = new Segment("id1","select * from test", "client1");
-        cache.put(segment.getId(),mapper.toBinaryObject(ignite.binary(),segment)); //PUT ONLY IN FIRST RUN
+        Ignite ignite = Ignition.start("ignite_client.xml");
+
+        IgniteCache<String,BinaryObject> cache = ignite.getOrCreateCache("DATA_CACHE");
+
+        Segment segment = new Segment("id1","query1", "client1");
+        SegmentBinaryMapper mapper = new SegmentBinaryMapper();
+        BinaryObject binaryObject = mapper.toBinaryObject(ignite.binary(),segment);
+
+        cache.put(segment.getId(),binaryObject); //PUT ONLY IN FIRST RUN
+
         System.out.println(mapper.fromBinaryObject(cache.get(segment.getId()))); //PASSING
+
         cache.loadCache(null); //FAILING
-        cache.loadCache(null, "select * from test.segment"); //FAILING
+
         System.out.printf("");
     }
 }
